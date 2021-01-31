51b9ab2a737ca5637f52fca941edf962fb785cbe
m3tro1d <qazwf734@gmail.com>
2021-01-31 18:37:02 +0300

    Add userdata controller

---
+++ api/controllers/userdata.js
@@ -0,0 +1,39 @@
+module.exports.getTasks = (req, res, next) => {
+  res.end('Get tasks');
+}
+
+module.exports.getOneTask = (req, res, next) => {
+  res.end('Get one task');
+}
+
+module.exports.addTask = (req, res, next) => {
+  res.end('Add task');
+}
+
+module.exports.updateTask = (req, res, next) => {
+  res.end('Update task');
+}
+
+module.exports.deleteTask = (req, res, next) => {
+  res.end('Delete task');
+}
+
+module.exports.getVirtues = (req, res, next) => {
+  res.end('Get virtues');
+}
+
+module.exports.getOneVirtue = (req, res, next) => {
+  res.end('Get one virtue');
+}
+
+module.exports.addVirtue = (req, res, next) => {
+  res.end('Add virtue');
+}
+
+module.exports.updateVirtue = (req, res, next) => {
+  res.end('Update virtue');
+}
+
+module.exports.deleteVirtue = (req, res, next) => {
+  res.end('Delete virtue');
+}
