ced58e5019d4e8844c312f0a79bc6c54114aafe3
m3tro1d <qazwf734@gmail.com>
2021-01-31 19:40:42 +0300

    Implement authorization

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 
 const User = mongoose.model('User');
@@ -48,7 +49,48 @@ module.exports.register = (req, res, next) => {
 }
 
 module.exports.login = (req, res, next) => {
-  res.end('Login');
+  const { email, password } = req.body;
+
+  // Check if all data is present
+  if (!email || !password) {
+    res.status(400).end('Please enter email and password');
+  }
+
+  // Check if the user exists
+  User.findOne({ email })
+    .then(user => {
+      if (!user) {
+        return res.status(400).end('User does not exist');
+      }
+
+      // Validate user's password
+      bcrypt.compare(password, user.password)
+        .then(isMatch => {
+          if (!isMatch) {
+            res.status(400).end('Invalid credentials');
+          }
+
+          // Create a token
+          jwt.sign(
+            { id: user.id },
+            process.env.JWT_SECRET,
+            (err, token) => {
+              if (err) {
+                throw err;
+              }
+              // Respond with the token and user info
+              res.json({
+                token,
+                user: {
+                  id: user.id,
+                  name: user.name,
+                  email: user.email
+                }
+              });
+            }
+          );
+        });
+    });
 }
 
 module.exports.getUser = (req, res, next) => {
