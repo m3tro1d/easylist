be007ee3f687d8a6527ac558b7e8bda0749bbee7
m3tro1d <qazwf734@gmail.com>
2021-02-01 16:36:15 +0300

    Implement getVirtues controller

--- api/controllers/userdata.js
+++ api/controllers/userdata.js
@@ -1,5 +1,23 @@
+const mongoose = require('mongoose');
+
+
+const Userdata = mongoose.model('Userdata');
+const User = mongoose.model('User');
+
 module.exports.getVirtues = (req, res, next) => {
-  res.end('Get virtues');
+  User.findById(req.user.id)
+    .then(user => {
+      // Check if user exists
+      if (!user) {
+        res.status(400).end('User does not exist');
+      }
+
+      // Find and return user's virtues
+      Userdata.findById(user.data_id)
+        .then(userdata => {
+          res.json(userdata.virtues);
+        });
+    });
 }
 
 module.exports.addVirtue = (req, res, next) => {
