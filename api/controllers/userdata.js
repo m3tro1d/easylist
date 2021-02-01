91324a192aa4bf86f11e726e345e40c780ec51d3
m3tro1d <qazwf734@gmail.com>
2021-02-01 16:59:34 +0300

    Implement virtue addition

--- api/controllers/userdata.js
+++ api/controllers/userdata.js
@@ -21,7 +21,34 @@ module.exports.getVirtues = (req, res, next) => {
 }
 
 module.exports.addVirtue = (req, res, next) => {
-  res.end('Add virtue');
+  const { name, task } = req.body;
+
+  // Check if all data is present
+  if (!name || !task) {
+    res.status(400).end('Please enter all fields');
+  } else {
+    // Find the user
+    User.findById(req.user.id)
+      .then(user => {
+        // Check if user exists
+        if (!user) {
+          res.status(400).end('User does not exist');
+        } else {
+          // Find user's data and add a virtue
+          Userdata.findById(user.data_id)
+            .then(userdata => {
+              const newVirtue = { name, task }
+              userdata.virtues.push(newVirtue);
+              userdata.save()
+                .then(createdUserdata => {
+                  res.status(201).json(
+                    createdUserdata.virtues[createdUserdata.virtues.length - 1]
+                  );
+                });
+            });
+        }
+      });
+  }
 }
 
 module.exports.updateVirtue = (req, res, next) => {
