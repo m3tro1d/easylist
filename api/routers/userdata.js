04670a12ce1cf0b7959f1b2ba4fd023496fedc12
m3tro1d <qazwf734@gmail.com>
2021-02-03 17:00:22 +0300

    Use the getters in the userdata router

--- api/routers/userdata.js
+++ api/routers/userdata.js
@@ -1,19 +1,19 @@
 const express = require('express');
 
-// Controller goes here
 const ctrlUserdata = require('../controllers/userdata.js');
 const auth = require('../../middleware/auth');
+const getters = require('../../middleware/getters');
 
 // Router for the blog API routes
 const router = express.Router();
 
 // Get user's virtues
-router.get('/virtues', auth, ctrlUserdata.getUserById, ctrlUserdata.getVirtues);
+router.get('/virtues', auth, getters.getUserById, ctrlUserdata.getVirtues);
 // Add user's virtue
-router.post('/virtues', auth, ctrlUserdata.getUserById, ctrlUserdata.addVirtue);
+router.post('/virtues', auth, getters.getUserById, ctrlUserdata.addVirtue);
 // Update user's virtue
-router.put('/virtues/:id', auth, ctrlUserdata.getUserById, ctrlUserdata.getVirtueById, ctrlUserdata.updateVirtue);
+router.put('/virtues/:id', auth, getters.getUserById, getters.getVirtueById, ctrlUserdata.updateVirtue);
 // Delete user's virtue
-router.delete('/virtues/:id', auth, ctrlUserdata.getUserById, ctrlUserdata.getVirtueById, ctrlUserdata.deleteVirtue);
+router.delete('/virtues/:id', auth, getters.getUserById, getters.getVirtueById, ctrlUserdata.deleteVirtue);
 
 module.exports = router;
