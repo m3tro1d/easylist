f259272826bf6b9dacc231dfc86672cfa27ddb0a
m3tro1d <qazwf734@gmail.com>
2021-02-03 16:58:13 +0300

    Fix the email registration message

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -42,8 +42,7 @@ module.exports.register = (req, res, next) => {
                       `Do Not Reply easylist <${process.env.VER_ADDRESS}>`,
                       email,
                       'Confirm your registration on easylist',
-                      `To confirm your registration on easylist please click\
-                      this link:\n${req.protocol}://${req.hostname}/api/users/register/confirm?token=${token}`,
+                      `To confirm your registration on easylist please click this link:\n${req.protocol}://${req.hostname}/api/users/register/confirm?token=${token}`,
                       (err, info) => {
                         if (err) { // Check for error
                           sendJsonResponse(res, 400, err);
