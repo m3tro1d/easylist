e22ee8573fbc17b2ed018c62725221bcaa7cab4e
m3tro1d <qazwf734@gmail.com>
2021-02-03 13:40:42 +0300

    Users controller refactoring

--- api/controllers/users.js
+++ api/controllers/users.js
@@ -10,136 +10,170 @@ const User = mongoose.model('User');
 module.exports.register = (req, res, next) => {
   const { email, password } = req.body;
 
-  // Check if all data is present
-  if (!email || !password) {
-    return res.status(400).end('Please enter all fields');
-  }
-
-  // Check if the user exists
-  User.findOne({ email })
-    .then(user => {
-      if (user) {
-        return res.status(400).end('User already exists');
-      } else {
-        // Hash user's password
-        bcrypt.genSalt(10, (err, salt) => {
-          bcrypt.hash(password, salt, (err, hash) => {
-            if (err) {
-              throw err;
-            }
-            // Get the confirmation token
-            jwt.sign({ email: email, password: hash }, process.env.VER_JWT_SECRET, { expiresIn: '1d' }, (err, token) => {
-              if (err) {
-                throw err
-              }
-              sendConfirmation(
-                `Do Not Reply easylist <${process.env.VER_ADDRESS}>`,
-                email,
-                'Confirm your registration on easylist',
-                `To confirm your registration on easylist please click this link:\n${req.protocol}://${req.hostname}/api/users/register/confirm?token=${token}`,
-                (err, info) => {
-                  if (err) {
-                    return res.status(400).end('Registration failed');
-                  } else {
-                    return res.status(201).end('Confirmation email has been sent');
+  if (!email || !password) { // Check if all data is present
+    sendJsonResponse(res, 400, {
+      message: 'Please enter all fields'
+    });
+  } else {
+    User
+      .findOne({ email })
+      .exec((err, user) => {
+        if (user) { // Check if the user already exists
+          sendJsonResponse(res, 400, {
+            message: 'User already exists'
+          });
+        } else if (err) { // Check for error
+          sendJsonResponse(res, 400, err);
+        } else {          // Hash user's password
+          bcrypt.genSalt(10, (err, salt) => {
+            bcrypt.hash(password, salt, (err, hash) => {
+              if (err) {  // Check for error
+                sendJsonResponse(res, 400, err);
+              } else {    // Generate the registration token
+                jwt.sign(
+                  { email: email, password: hash },
+                  process.env.VER_JWT_SECRET,
+                  { expiresIn: '1d' },
+                  (err, token) => {
+                  if (err) { // Check for error
+                    sendJsonResponse(res, 400, err);
+                  } else {   // Send a confirmation
+                    sendConfirmation(
+                      `Do Not Reply easylist <${process.env.VER_ADDRESS}>`,
+                      email,
+                      'Confirm your registration on easylist',
+                      `To confirm your registration on easylist please click
+                      this link:\n${req.protocol}://${req.hostname}/api/users/register/confirm?token=${token}`,
+                      (err, info) => {
+                        if (err) { // Check for error
+                          sendJsonResponse(res, 400, err);
+                        } else {
+                          sendJsonResponse(res, 201, {
+                            message: 'Confirmation email has been sent'
+                          });
+                        }
+                      });
                   }
                 });
+              }
             });
           });
-        });
-      }
-    });
+        }
+      });
+  }
 }
 
 module.exports.registerConfirm = (req, res, next) => {
-  // Check if token is present
-  if (!req.query.token) {
-    return res.status(400).end('No token provided');
-  }
-
-  try {
-    // Verify the token
-    const newUserPlain = jwt.verify(req.query.token, process.env.VER_JWT_SECRET);
-
-    // Check if the user has already been registered
-    User.findOne({ email: newUserPlain.email })
-      .then(user => {
-        if (user) {
-          return res.status(400).end('User already registered');
-        } else {
-          // Save the user and user's data
-          const newUserdata = Userdata();
-          const newUser = User({
-            email: newUserPlain.email,
-            password: newUserPlain.password,
-            data_id: newUserdata.id
-          });
-          newUser.save()
-            .then(user => {
-              newUserdata.save()
-                .then(userdata => {
-                  // Redirect to the index page
-                  return res.redirect(302, '/');
-                });
+  if (!req.query.token) {  // Check if token is present
+    sendJsonResponse(res, 400, {
+      message: 'No token provided'
+    });
+  } else {
+    try {
+      // Verify the token
+      const newUserPlain = jwt.verify(req.query.token, process.env.VER_JWT_SECRET);
+      User
+        .findOne({ email: newUserPlain.email })
+        .exec((err, user) => {
+          if (user) {       // Check if the user has been registered already
+            sendJsonResponse(res, 400, {
+              message: 'User already registered'
             });
-        }
+          } else if (err) { // Check for error
+            sendJsonResponse(res, 400, err);
+          } else {          // Create the user and user's data
+            Userdata
+              .create({}, (err, userdata) => {
+                if (err) { // Check for error
+                  sendJsonResponse(res, 400, err);
+                } else {   // Create the user
+                  User
+                    .create({
+                      email: newUserPlain.email,
+                      password: newUserPlain.password,
+                      data_id: userdata.id
+                    }, (err, user) => {
+                      if (err) { // Check for error
+                        sendJsonResponse(res, 400, err);
+                      } else {   // Redirect to the index page
+                        res.redirect(302, '/');
+                      }
+                    });
+                }
+              });
+          }
+        });
+    } catch(e) { // Catch the invalid token
+      sendJsonResponse(res, 400, {
+        message: 'Token is invalid'
       });
-  } catch(e) {
-    return res.status(400).end('Token is invalid');
+    }
   }
 }
 
 module.exports.login = (req, res, next) => {
   const { email, password } = req.body;
 
-  // Check if all data is present
-  if (!email || !password)
-    return res.status(400).end('Please enter email and password');
-
-  // Check if the user exists
-  User.findOne({ email })
-    .then(user => {
-      if (!user) {
-        return res.status(400).end('User does not exist');
-      } else {
-        // Validate user's password
-        bcrypt.compare(password, user.password)
-          .then(isMatch => {
-            if (!isMatch) {
-              return res.status(400).end('Invalid credentials');
-            } else {
-              // Create a token
-              jwt.sign(
-                { id: user.id },
-                process.env.JWT_SECRET,
-                (err, token) => {
-                  if (err) {
-                    throw err;
-                  }
-                  // Respond with the token and user info
-                  return res.json({
-                    token,
-                    user: {
-                      id: user.id,
-                      email: user.email,
-                      data_id: user.data_id
-                    }
-                  });
-                }
-              );
-            }
-          });
-      }
+  if (!email || !password) {   // Check if all data is present
+    sendJsonResponse(res, 400, {
+      message: 'Please enter email and password'
     });
+  } else {
+    User
+      .findOne({ email })
+      .exec((err, user) => {
+        if (!user) {            // Check if the user exists
+          sendJsonResponse(res, 404, {
+            message: 'User not found'
+          });
+        } else if (err) {       // Check for error
+          sendJsonResponse(res, 400, err);
+        } else {
+          bcrypt.compare(password, user.password)
+            .then(isMatch => {
+              if (!isMatch) {   // Check if the password is correct
+                sendJsonResponse(res, 400, {
+                  message: 'Invalid credentials'
+                });
+              } else {
+                // Create a token
+                jwt.sign(
+                  { id: user.id },
+                  process.env.JWT_SECRET,
+                  (err, token) => {
+                    if (err) {   // Check for error
+                      sendJsonResponse(res, 400, err);
+                    } else {     // Respond with the token and user info
+                      sendJsonResponse(res, 200, {
+                        token,
+                        user: {
+                          id: user.id,
+                          email: user.email,
+                          data_id: user.data_id
+                        }
+                      });
+                    }
+                  }
+                );
+              }
+            });
+        }
+      });
+  }
 }
 
 module.exports.getUser = (req, res, next) => {
-  User.findById(req.user.id)
-    .then(user => {
+  User
+    .findById(req.user.id)
+    .exec((err, user) => {
       if (!user) {
-        return res.status(400).end('User does not exist');
+        return sendJsonResponse(res, 404, {
+          message: 'User not found'
+        });
+      } else if (err) {
+        sendJsonResponse(res, 400, err);
       } else {
-        res.json({
+        sendJsonResponse(res, 200, {
           id: user.id,
           email: user.email,
           data_id: user.data_id
@@ -150,6 +184,12 @@ module.exports.getUser = (req, res, next) => {
 
 
 // Some useful functions
+// Ends res with given status and json content
+function sendJsonResponse(res, status, content) {
+  res.status(status).json(content);
+}
+
+// Sends a registration confirmation email
 function sendConfirmation(from, to, subject, text, callback) {
   // Set up the mail client
   const emailer = nodemailer.createTransport({
