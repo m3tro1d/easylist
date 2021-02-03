a56f70b537fb76670896fa158ce8483558974f25
m3tro1d <qazwf734@gmail.com>
2021-02-03 16:41:11 +0300

    Refactor auth middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,18 +1,29 @@
 const jwt = require('jsonwebtoken');
 
-module.exports= (req, res, next) => {
+// Authorization middleware
+module.exports = (req, res, next) => {
   const token = req.header('x-auth-token');
 
-  // Check if token is present
-  if (!token) {
-    return res.status(401).end('No token, authorization denied');
+  if (!token) { // Check if token is present
+    sendJsonResponse(res, 401, {
+      message: 'No token, authorization denied'
+    });
+  } else { // Proceed and decode the token
+    try {
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      req.user = decoded;
+      next();
+    } catch(e) {
+      sendJsonResponse(res, 400, {
+        message: 'Token is invalid'
+      });
+    }
   }
+}
 
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded;
-    next();
-  } catch(e) {
-    return res.status(400).end('Token is invalid');
-  }
+
+// Some useful functions
+// Ends res with given status and json content
+function sendJsonResponse(res, status, content) {
+  res.status(status).json(content);
 }
