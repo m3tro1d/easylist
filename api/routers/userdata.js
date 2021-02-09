fa097449681e7808ed68b278361d8ca1b34de5df
m3tro1d <qazwf734@gmail.com>
2021-02-09 11:02:27 +0300

    Change userdata routes to use name instead of ID

--- api/routers/userdata.js
+++ api/routers/userdata.js
@@ -10,17 +10,17 @@ const router = express.Router();
 // Get user's virtues
 router.get('/virtues', auth, getters.getUserById, ctrlUserdata.getVirtues);
 // Get one virtue
-router.get('/virtues/:virtueId', auth, getters.getUserById, getters.getVirtueById, ctrlUserdata.getOneVirtue);
+router.get('/virtues/:virtueName', auth, getters.getUserById, getters.getVirtueById, ctrlUserdata.getOneVirtue);
 // Add user's virtue
 router.post('/virtues', auth, getters.getUserById, ctrlUserdata.addVirtue);
 // Update user's virtue
-router.put('/virtues/:virtueId', auth, getters.getUserById, getters.getVirtueById, ctrlUserdata.updateVirtue);
+router.put('/virtues/:virtueName', auth, getters.getUserById, getters.getVirtueById, ctrlUserdata.updateVirtue);
 // Delete user's virtue
-router.delete('/virtues/:virtueId', auth, getters.getUserById, getters.getVirtueById, ctrlUserdata.deleteVirtue);
+router.delete('/virtues/:virtueName', auth, getters.getUserById, getters.getVirtueById, ctrlUserdata.deleteVirtue);
 
 // Add a task in a virtue
-router.post('/virtues/:virtueId/tasks/', auth, getters.getUserById, getters.getVirtueById, ctrlUserdata.addTask);
+router.post('/virtues/:virtueName/tasks/', auth, getters.getUserById, getters.getVirtueById, ctrlUserdata.addTask);
 // Delete a task
-router.delete('/virtues/:virtueId/tasks/:taskId', auth, getters.getUserById, getters.getVirtueById, ctrlUserdata.deleteTask);
+router.delete('/virtues/:virtueName/tasks/:taskId', auth, getters.getUserById, getters.getVirtueById, ctrlUserdata.deleteTask);
 
 module.exports = router;
