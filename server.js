21d4aa2b32c16519c46055ad9dfad3bf01cfdbb1
m3tro1d <qazwf734@gmail.com>
2021-02-13 09:26:19 +0300

    Add CORS handling

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const logger = require('morgan');
 const createError = require('http-errors');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 const http = require('http');
 const path = require('path');
 
@@ -24,6 +25,7 @@ app.use(logger('dev'));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors());
 
 // API routes
 app.use('/api/users', usersRouter);
